8ecd21e311cb811926e6efe94843778c8797271d
Srivi Balaji <srivibalaji@utexas.edu>
2025-10-06 18:27:10 -0500

    added mlp

--- models/__init__.py
+++ models/__init__.py
@@ -2,10 +2,12 @@ from .lr import LR
 from .cnn import CNN
 from .wideresnet import WideResNet
 from .lstm import LSTM
+from .mlp import MLP
 
 Models = {
     'lr': LR,
     'cnn': CNN,
     'wideresnet': WideResNet,
-    'lstm': LSTM
+    'lstm': LSTM,
+    'mlp': MLP
 }
---
+++ models/mlp.py
@@ -0,0 +1,41 @@
+"""Three-Layer Fully Connected Network"""
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class MLP(nn.Module):
+    def __init__(self, in_shape=None, out_dim=10, hidden_dim=256):
+        super(MLP, self).__init__()
+
+        # stole from lr
+        if in_shape is None:
+            in_dim = 3072
+        elif type(in_shape) == int:
+            # D
+            in_dim = in_shape
+        elif len(in_shape) == 2:
+            # B x D
+            in_dim = in_shape[1]
+        else:
+            # B x C x H x W
+            in_dim = in_shape[1] * in_shape[2] * in_shape[3]
+
+        self.fc1 = nn.Linear(in_dim, hidden_dim)
+        self.fc2 = nn.Linear(hidden_dim, hidden_dim)
+        self.fc3 = nn.Linear(hidden_dim, out_dim)
+
+        self.dropout = nn.Dropout(0.2)
+        self.embeddings = None
+
+    def forward(self, x):
+        x = torch.flatten(x, start_dim=1)
+
+        x = F.relu(self.fc1(x))
+        x = self.dropout(x)
+        x = F.relu(self.fc2(x))
+
+        self.embeddings = x.clone().detach()
+
+        out = self.fc3(x)
+        return out
