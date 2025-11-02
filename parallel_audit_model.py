c463ccf64e354827933aea47b774f5d6bacd7abe
pineappleEnthusiast <44884695+pineappleEnthusiast@users.noreply.github.com>
2025-11-02 11:06:09 -0500

    added barrier + nccl + destroy process group

--- parallel_audit_model.py
+++ parallel_audit_model.py
@@ -449,13 +449,9 @@ def main():
         init_method='env://'
     )
     
-    world_size = dist.get_world_size()
-    rank = dist.get_rank()
-    
-    # Get local rank (GPU on this node)
-    local_rank = args.local_rank
-    if 'LOCAL_RANK' in os.environ:
-        local_rank = int(os.environ['LOCAL_RANK'])
+    local_rank = int(os.environ.get('LOCAL_RANK', 0))
+    rank = int(os.environ.get('RANK', 0))
+    world_size = int(os.environ.get('WORLD_SIZE', 1))
     
     torch.cuda.set_device(local_rank)
     
